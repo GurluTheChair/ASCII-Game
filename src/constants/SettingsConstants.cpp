28cc5ea26ada5c69caa735254c3c0ac4effcdae0
Alex Vu <alex.vu.auditeur@lecnam.net>
2022-10-09 18:13:17 +0200

    make game more generous

--- src/constants/SettingsConstants.cpp
+++ src/constants/SettingsConstants.cpp
@@ -3,7 +3,7 @@
 namespace GameplaySettingsConstants {
 	const unsigned int SCORE_MULTIPLIER = 100;
 	const float EARLY_PRESS_TOLERANCE_SECONDS = 0.1f;
-	const float LATE_PRESS_TOLERANCE_SECONDS = 0.1f;
-	const float EARLY_RELEASE_TOLERANCE_SECONDS = 0.15f;
+	const float LATE_PRESS_TOLERANCE_SECONDS = 0.15f;
+	const float EARLY_RELEASE_TOLERANCE_SECONDS = 0.2f;
 	const float MAX_MISS_TIME_DISTANCE_SECONDS = 0.1f;
 }
