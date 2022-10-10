fea5a3ea1c0260e4dd594721bd9ef7cef0367e20
Alex Vu <alex.vu.auditeur@lecnam.net>
2022-10-10 18:22:46 +0200

    make small tweaks to make gameplay easier

--- src/constants/InputConstants.cpp
+++ src/constants/InputConstants.cpp
@@ -3,7 +3,7 @@
 #include "InputConstants.h"
 
 namespace KeyConstants {
-	const char LANE_KEYS[GAME_LANE_COUNT] = { 'A', 'Z', 'E', 'R' };
+	const char LANE_KEYS[GAME_LANE_COUNT] = { 'S', 'D', 'F', 'G' };
 	const char MENU_PREVIOUS = VK_UP;
 	const char MENU_NEXT = VK_DOWN;
 	const char MENU_CONFIRM = VK_RETURN;
